c0a523af2ed5504c8a64ad80fbfa8afc33a59663
Emanuele Favero <emafaux89@gmail.com>
2025-08-28 20:55:03 +0200

    feat: add server and client urls

---
+++ frontend/src/config/baseUrl.ts
@@ -0,0 +1,8 @@
+export const serverUrl =
+  process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:8000'
+
+export const clientUrl =
+  process.env.NEXT_PUBLIC_CLIENT_URL || 'http://localhost:3000'
+
+export const apiUrl = `${serverUrl}/api`
+export const todoApiUrl = `${apiUrl}/todo/`
