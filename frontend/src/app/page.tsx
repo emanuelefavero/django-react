df5640408d0545330c5c478caf38b52804ad9de0
Emanuele Favero <emafaux89@gmail.com>
2025-08-28 21:10:46 +0200

    feat: fetch todos from client

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,34 @@
-// TODO Add `baseUrl` util to get the server base url
-// TODO Fetch posts from `http://localhost:8000/api/todo/`
+import { todoApiUrl } from '@/config/baseUrl'
+import type { Todo } from '@/types/todo'
+import { Suspense } from 'react'
 
-export default function Home() {
+async function fetchTodos() {
+  const response = await fetch(todoApiUrl)
+  if (!response.ok) throw new Error('Failed to fetch todos')
+
+  return response.json()
+}
+
+export default async function Home() {
+  const todos = await fetchTodos()
+
   return (
     <>
       <h1 className='text-pink-500'>Hello</h1>
+
+      <Suspense fallback={<div>Loading...</div>}>
+        <ul>
+          {todos.map((todo: Todo) => (
+            <li key={todo.id}>
+              <div>
+                {todo.id} - <span className='font-semibold'>{todo.title}</span>
+              </div>
+              <div className='text-gray-500'>Created: {todo.created}</div>
+              <div className='text-gray-500'>Updated: {todo.updated}</div>
+            </li>
+          ))}
+        </ul>
+      </Suspense>
     </>
   )
 }
