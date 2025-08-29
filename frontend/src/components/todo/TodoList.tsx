1d8ac7060d80a558af07094109174e1aa8996e63
Emanuele Favero <emafaux89@gmail.com>
2025-08-29 18:10:44 +0200

    feat: add cn to TodoList component

--- frontend/src/components/todo/TodoList.tsx
+++ frontend/src/components/todo/TodoList.tsx
@@ -1,31 +1,32 @@
+import { cn } from '@/lib/utils'
 import type { Todo } from '@/types/todo'
+
+type Props = React.ComponentPropsWithRef<'ul'> & {
+  todos: Todo[]
+}
+
+export default function TodoList({ todos, className, ...props }: Props) {
+  return (
+    <ul className={cn(className)} {...props}>
+      {todos.map((todo) => (
+        <TodoItem key={todo.id} todo={todo} />
+      ))}
+    </ul>
+  )
+}
 
 type TodoItemProps = React.ComponentPropsWithRef<'li'> & {
   todo: Todo
 }
 
-function TodoItem({ todo }: TodoItemProps) {
+function TodoItem({ todo, className, ...props }: TodoItemProps) {
   return (
-    <li>
+    <li className={cn(className)} {...props}>
       <div>
         <span className='font-semibold'>{todo.title}</span>
       </div>
       <div className='text-gray-500'>Created: {todo.created}</div>
       <div className='text-gray-500'>Updated: {todo.updated}</div>
     </li>
-  )
-}
-
-type Props = React.ComponentPropsWithRef<'ul'> & {
-  todos: Todo[]
-}
-
-export default function TodoList({ todos, ...props }: Props) {
-  return (
-    <ul {...props}>
-      {todos.map((todo) => (
-        <TodoItem key={todo.id} todo={todo} />
-      ))}
-    </ul>
   )
 }
