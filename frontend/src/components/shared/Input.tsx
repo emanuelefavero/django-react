2772eab1434f1c400e3887e72f3c4d6e5692990f
Emanuele Favero <emafaux89@gmail.com>
2025-08-29 22:41:52 +0200

    feat: add Input light mode style

--- frontend/src/components/shared/Input.tsx
+++ frontend/src/components/shared/Input.tsx
@@ -3,7 +3,13 @@ import { cn } from '@/lib/utils'
 type Props = React.ComponentPropsWithRef<'input'> & {}
 
 export default function Input({ className, ...props }: Props) {
-  const baseStyles = `border border-neutral-700 bg-neutral-900 text-sm rounded-lg px-2 py-1 focus:outline-none focus-visible:bg-transparent placeholder:text-neutral-500 transition duration-200`
-
-  return <input className={cn(baseStyles, className)} {...props} />
+  return (
+    <input
+      className={cn(
+        'rounded-lg border border-neutral-400 bg-neutral-100 px-2 py-1 text-sm transition duration-200 placeholder:text-neutral-400 focus:outline-none focus-visible:bg-transparent dark:border-neutral-700 dark:bg-neutral-900 dark:placeholder:text-neutral-500',
+        className,
+      )}
+      {...props}
+    />
+  )
 }
