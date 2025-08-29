d66bcaac07738eb0c4af58101c1aefddddc5cb3e
Emanuele Favero <emafaux89@gmail.com>
2025-08-29 23:38:29 +0200

    feat: add invalid, focus and hover states to Input

--- frontend/src/components/shared/Input.tsx
+++ frontend/src/components/shared/Input.tsx
@@ -6,7 +6,7 @@ export default function Input({ className, ...props }: Props) {
   return (
     <input
       className={cn(
-        'rounded-lg border border-neutral-400 bg-neutral-100 px-2 py-1 text-sm transition duration-200 placeholder:text-neutral-400 focus:outline-none focus-visible:bg-transparent dark:border-neutral-700 dark:bg-neutral-900 dark:placeholder:text-neutral-500',
+        'rounded-lg border border-neutral-400 bg-neutral-100 px-2 py-1 text-sm transition duration-200 placeholder:text-neutral-400 hover:not-focus:border-neutral-500 focus:outline-none focus-visible:bg-transparent focus-visible:invalid:border-(--primary) disabled:opacity-50 dark:border-neutral-700 dark:bg-neutral-900 dark:placeholder:text-neutral-500 dark:hover:not-focus:border-neutral-600',
         className,
       )}
       {...props}
