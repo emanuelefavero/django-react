0ef755c5797d81f6944251d0c37e0e7b00abd692
Emanuele Favero <emafaux89@gmail.com>
2025-09-03 02:34:29 +0200

    docs: add TODO comments

--- frontend/src/components/shared/Test.tsx
+++ frontend/src/components/shared/Test.tsx
@@ -4,6 +4,42 @@ import Button from '@/components/shared/Button'
 import { errorToastClassNames, successToastClassNames } from '@/styles/sonner'
 import { toast } from 'sonner'
 
+// TODO style toasts (to style default toast, @see @/components/ui/sonner.tsx)
+/*
+TODO add a `@/components/shared/Toast.tsx` component that wraps sonner's `toast` functions and export it with variants "default", "success", and "error"
+
+// EXAMPLE:
+import { toast } from 'sonner'
+import { successToastClassNames, errorToastClassNames } from '@/styles/sonner'
+
+export type ToastOptions = Parameters<typeof toast>[1]
+export type ToastVariant = 'default' | 'success' | 'error'
+
+export interface CustomToastProps extends ToastOptions {
+  variant?: ToastVariant
+  title: string
+}
+
+export function Toast({ variant = 'default', title, classNames, ...options }: CustomToastProps) {
+  if (variant === 'success') {
+    return toast.success(title, {
+      classNames: classNames ?? successToastClassNames,
+      ...options,
+    })
+  }
+  if (variant === 'error') {
+    return toast.error(title, {
+      classNames: classNames ?? errorToastClassNames,
+      ...options,
+    })
+  }
+  return toast(title, {
+    classNames,
+    ...options,
+  })
+}
+*/
+
 export default function Test() {
   return (
     <div className='mb-4 flex gap-2'>
