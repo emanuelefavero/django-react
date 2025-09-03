74ea03d06d723d5c751c92507cfff7a6eccd32ca
Emanuele Favero <emafaux89@gmail.com>
2025-09-03 17:40:38 +0200

    feat: allow to pass classNames to toast in lib/sonner

--- frontend/src/components/shared/Test.tsx
+++ frontend/src/components/shared/Test.tsx
@@ -1,51 +1,7 @@
 'use client'
 
 import Button from '@/components/shared/Button'
-import {
-  defaultToastClassNames,
-  errorToastClassNames,
-  successToastClassNames,
-} from '@/styles/sonner'
-import { toast } from 'sonner'
-
-/*
-TODO add a `@/components/shared/Toast.tsx` component that wraps sonner's `toast` functions and export it with variants "default", "success", and "error"
-
-// EXAMPLE:
-import { toast } from 'sonner'
-import {
-  defaultToastClassNames,
-  errorToastClassNames,
-  successToastClassNames,
-} from '@/styles/sonner'
-
-export type ToastOptions = Parameters<typeof toast>[1]
-export type ToastVariant = 'default' | 'success' | 'error'
-
-export interface CustomToastProps extends ToastOptions {
-  variant?: ToastVariant
-  title: string
-}
-
-export function Toast({ variant = 'default', title, classNames, ...options }: CustomToastProps) {
-  if (variant === 'success') {
-    return toast.success(title, {
-      classNames: classNames ?? successToastClassNames,
-      ...options,
-    })
-  }
-  if (variant === 'error') {
-    return toast.error(title, {
-      classNames: classNames ?? errorToastClassNames,
-      ...options,
-    })
-  }
-  return toast(title, {
-    classNames: classNames ?? defaultToastClassNames,
-    ...options,
-  })
-}
-*/
+import { toast } from '@/lib/sonner'
 
 export default function Test() {
   return (
@@ -53,8 +9,6 @@ export default function Test() {
       <Button
         onClick={() => {
           toast('Default Toast', {
-            classNames: defaultToastClassNames,
-
             description: 'This is a test toast',
             duration: 60000,
             action: {
@@ -77,8 +31,6 @@ export default function Test() {
       <Button
         onClick={() => {
           toast.success('Success Toast', {
-            classNames: successToastClassNames,
-
             description: 'This is a test toast',
             duration: 60000,
             action: {
@@ -101,8 +53,6 @@ export default function Test() {
       <Button
         onClick={() => {
           toast.error('Error Toast', {
-            classNames: errorToastClassNames,
-
             description: 'This is a test toast',
             duration: 60000,
             action: {
