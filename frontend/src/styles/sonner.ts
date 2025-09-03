4abab630068982c949980773c053071e1b428baf
Emanuele Favero <emafaux89@gmail.com>
2025-09-03 15:20:12 +0200

    feat: add border to error and success toasts

--- frontend/src/styles/sonner.ts
+++ frontend/src/styles/sonner.ts
@@ -1,7 +1,7 @@
 export const errorToastClassNames = {
   toast: /*tw*/ '!bg-error',
   actionButton:
-    /*tw*/ '!bg-error !border-error-foreground !text-error-foreground hover:!bg-error-foreground hover:!text-error',
+    /*tw*/ '!bg-error !border !border-error-foreground !text-error-foreground hover:!bg-error-foreground hover:!text-error',
   cancelButton: /*tw*/ '!bg-error',
   icon: /*tw*/ '!text-error-foreground',
 
@@ -13,7 +13,7 @@ export const errorToastClassNames = {
 export const successToastClassNames = {
   toast: /*tw*/ '!bg-success',
   actionButton:
-    /*tw*/ '!bg-success !border-success-foreground !text-success-foreground hover:!bg-success-foreground hover:!text-success',
+    /*tw*/ '!bg-success !border !border-success-foreground !text-success-foreground hover:!bg-success-foreground hover:!text-success',
   cancelButton: /*tw*/ '!bg-success',
   icon: /*tw*/ '!text-success-foreground',
 
