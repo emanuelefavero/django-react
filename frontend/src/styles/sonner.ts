c6b37bbfaa4acf7d2c0c0bfac5aad93c68b4618d
Emanuele Favero <emafaux89@gmail.com>
2025-09-03 15:03:26 +0200

    feat: style success toast

--- frontend/src/styles/sonner.ts
+++ frontend/src/styles/sonner.ts
@@ -5,13 +5,17 @@ export const errorToastClassNames = {
   actionButton: /*tw*/ '!bg-white/10',
   cancelButton: /*tw*/ '!bg-white/10',
   closeButton: /*tw*/ '!bg-white/10',
+  icon: /*tw*/ '!text-white',
 }
 
 export const successToastClassNames = {
-  toast: /*tw*/ '!bg-green-500',
-  title: /*tw*/ '!text-white',
-  description: /*tw*/ '!text-neutral-300',
-  actionButton: /*tw*/ '!bg-white/10',
-  cancelButton: /*tw*/ '!bg-white/10',
-  closeButton: /*tw*/ '!bg-white/10',
+  toast: /*tw*/ '!bg-success',
+  actionButton:
+    /*tw*/ '!bg-success !border-success-foreground !text-success-foreground hover:!bg-success-foreground hover:!text-success',
+  icon: /*tw*/ '!text-success-foreground',
+
+  // title: /*tw*/ '',
+  // description: /*tw*/ '',
+  // cancelButton: /*tw*/ '',
+  // closeButton: /*tw*/ '',
 }
