a2e4d676526e7101c3da79bb90354d82589ddce2
Emanuele Favero <emafaux89@gmail.com>
2025-09-04 15:20:13 +0200

    refactor: use tailwind custom styles on shared input

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -18,7 +18,7 @@ export default async function Home() {
   return (
     <>
       <div className='flex flex-col gap-4'>
-        <h1 className='text-pink-500'>Todo List</h1>
+        <h1 className='text-primary'>Todo List</h1>
         <AddTodo />
 
         <Suspense fallback={<div>Loading Todos...</div>}>
--- frontend/src/components/shared/Input.tsx
+++ frontend/src/components/shared/Input.tsx
@@ -3,16 +3,16 @@ import { cn } from '@/lib/utils'
 // TODO Fix input width on small screens
 
 const baseStyles = cn(
-  'rounded-lg border border-neutral-400 bg-neutral-100 px-2 py-1 text-sm transition duration-200 placeholder:text-neutral-400 disabled:opacity-50',
+  'rounded-lg border border-border bg-input px-2 py-1 text-sm transition duration-200 placeholder:text-placeholder disabled:opacity-50',
 
   // Hover
-  'hover:not-focus:border-neutral-500',
+  'hover:not-focus:border-border-hover',
 
   // Focus
-  'focus:outline-none focus-visible:bg-transparent focus-visible:not-invalid:border-emerald-600 focus-visible:invalid:border-pink-600',
+  'focus:outline-none focus-visible:bg-transparent focus-visible:not-invalid:border-success-foreground focus-visible:invalid:border-error-foreground',
 
   // Dark
-  'dark:border-neutral-700 dark:bg-neutral-900 dark:placeholder:text-neutral-500 dark:hover:not-focus:border-neutral-600 dark:focus-visible:not-invalid:border-emerald-700 dark:focus-visible:invalid:border-pink-800',
+  'dark:border-border dark:bg-input dark:placeholder:text-placeholder dark:hover:not-focus:border-border-hover dark:focus-visible:not-invalid:border-success-foreground dark:focus-visible:invalid:border-error-foreground',
 )
 
 type Props = React.ComponentPropsWithRef<'input'> & {}
