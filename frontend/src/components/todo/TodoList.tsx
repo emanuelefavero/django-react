6b6a12d017c617e7a5d65af0e3ff84e50b84548b
Emanuele Favero <emafaux89@gmail.com>
2025-09-11 08:06:18 +0200

    feat: add motion animation to todo list

--- frontend/src/components/todo/TodoList.tsx
+++ frontend/src/components/todo/TodoList.tsx
@@ -1,5 +1,8 @@
+'use client'
+
 import { cn } from '@/lib/utils'
 import type { Todo } from '@/types/todo'
+import { AnimatePresence, motion } from 'motion/react'
 import TodoItem from './TodoItem'
 
 type Props = React.ComponentPropsWithRef<'ul'> & {
@@ -9,9 +12,20 @@ type Props = React.ComponentPropsWithRef<'ul'> & {
 export default function TodoList({ todos, className, ...props }: Props) {
   return (
     <ul className={cn('flex max-w-sm flex-col gap-2', className)} {...props}>
-      {todos.map((todo) => (
-        <TodoItem key={todo.id} todo={todo} />
-      ))}
+      <AnimatePresence>
+        {todos.map((todo) => (
+          <motion.div
+            key={todo.id}
+            layout
+            initial={{ opacity: 0, y: -24 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 24 }}
+            transition={{ duration: 0.2 }}
+          >
+            <TodoItem todo={todo} />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </ul>
   )
 }
